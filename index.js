818284c85fc2a28e29981f6cf953d733d70442b5
Alan He <qianghe421@gmail.com>
2022-12-15 14:01:19 +0800

    init

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const Koa = require('koa');
 const app = new Koa();
 
 // response
+app.use(async (ctx, next) => {
+  // if(ctx.url === '/'){
+  //   next();
+  // }
+  try {
+    await new Promise((resolve, reject) => {
+      reject('helloworld');
+    });
+  } catch (e) {
+    ctx.status = 500;
+    ctx.body = e+'123';
+  }
+});
+
 app.use(ctx => {
+  ctx.status = 201;
   console.log(ctx.url);
-  ctx.body = 'Hello Koa';
+  ctx.body = '123455 Koa';
 });
 
 const server = app.listen(3000);
-console.log('koa-demo listening at http://127.0.0.1:%s', server.address().port);
+console.log('koa-demo listening at http://127.0.0.1:%s');
